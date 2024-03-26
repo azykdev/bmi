79d005eac8d4f9bc1289764ed0b4fe1be7632116
azykdev <azyk0106@gmail.com>
2024-03-27 00:15:05 +0500

    #1 - create project

---
+++ src/layouts/components/NavItems.vue
@@ -0,0 +1,113 @@
+<script setup>
+import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue';
+import VerticalNavGroup from '@layouts/components/VerticalNavGroup.vue';
+import VerticalNavLink from '@layouts/components/VerticalNavLink.vue';
+</script>
+
+<template>
+  <!-- 👉 Dashboards -->
+  <VerticalNavLink
+      :item="{
+        title: 'Dashboard',
+        to: '/dashboard',
+        icon: 'ri-dashboard-line',
+      }"
+    />
+
+  <!-- 👉 Front Pages -->
+  <!-- <VerticalNavGroup
+    :item="{
+      title: 'Front Pages',
+      icon: 'ri-file-copy-line',
+      badgeContent: 'Pro',
+      badgeClass: 'bg-light-primary text-primary',
+    }"
+  >
+    <VerticalNavLink
+      :item="{
+        title: 'Landing',
+        href: 'https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/demo-1/front-pages/landing-page',
+        target: '_blank',
+      }"
+    />
+    <VerticalNavLink
+      :item="{
+        title: 'Pricing',
+        href: 'https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/demo-1/front-pages/pricing',
+        target: '_blank',
+      }"
+    />
+    
+  </VerticalNavGroup> -->
+
+  <!-- 👉 Apps & Pages -->
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'Tender',
+    }"
+  />
+
+  <VerticalNavGroup
+    :item="{
+      title: 'Tender',
+      icon: 'ri-auction-line',
+    }"
+  >
+    <VerticalNavLink
+    :item="{
+        title: 'Yangi tender',
+        to: '/new-tender',
+      }"
+    />
+    <VerticalNavLink
+    :item="{
+        title: 'Tender g\'olibi',
+        to: '/victory-tender',
+      }"
+    />
+    <VerticalNavLink
+    :item="{
+        title: 'Tenderni yopish',
+        to: '/close-tender',
+      }"
+    />
+    
+    
+  </VerticalNavGroup>
+  
+
+  
+
+  <!-- 👉 User Interface -->
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'User Interface',
+    }"
+  />
+  
+  <VerticalNavLink
+    :item="{
+      title: 'Icons',
+      icon: 'ri-remixicon-line',
+      to: '/icons',
+    }"
+  />
+  
+
+  <!-- 👉 Forms & Tables -->
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'Forms & Tables',
+    }"
+  />
+  
+  
+  <!-- 👉 Others -->
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'Others',
+    }"
+  />
+  
+  
+</template>
---
+++ src/plugins/router/routes.js
@@ -0,0 +1,43 @@
+export const routes = [
+  { path: '/', redirect: '/dashboard' },
+  {
+    path: '/',
+    component: () => import('@/layouts/default.vue'),
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/pages/dashboard.vue'),
+      },
+      {
+        path: 'account-settings',
+        component: () => import('@/pages/account-settings.vue'),
+      },
+      
+      {
+        path: 'icons',
+        component: () => import('@/pages/icons.vue'),
+      },
+      
+      
+      
+    ],
+  },
+  {
+    path: '/',
+    component: () => import('@/layouts/blank.vue'),
+    children: [
+      {
+        path: 'login',
+        component: () => import('@/pages/login.vue'),
+      },
+      {
+        path: 'register',
+        component: () => import('@/pages/register.vue'),
+      },
+      {
+        path: '/:pathMatch(.*)*',
+        component: () => import('@/pages/[...error].vue'),
+      },
+    ],
+  },
+]
